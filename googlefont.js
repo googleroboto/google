c826f0e4b922e24564fc0af537a9a5cebe937b46
googleroboto <166996707+googleroboto@users.noreply.github.com>
2024-04-17 03:37:20 +0530

    Update googlefont.js

--- googlefont.js
+++ googlefont.js
@@ -1,34 +1,3 @@
-console.log('this_is_testing')
-
-const root = document.getElementById('root')
-
-root.style.color = "red";
-root.style.backgroundColor = "yellow";
-root.style.padding = "10px";
-
-// (function (w, d, s, o, f, js, fjs) {
-//     w["spotbus-chatbot"] = o;
-//     w[o] =
-//       w[o] ||
-//       function () {
-//         (w[o].q = w[o].q || []).push(arguments);
-//       };
-//     (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
-//     js.id = o;
-//     js.src = f;
-//     js.async = 1;
-//     fjs.parentNode.insertBefore(js, fjs);
-//     w.chatbotConfig = {
-//       token: "8fab2211f44a4a0993e2a8ca619a580f",
-//       userid: "6ba27e00-cac7-46d4-ade1-e0cc23f65bd4",
-//     };
-//     const divElement = d.createElement("div");
-//     divElement.id = "spotbus-chatbot-root";
-//     d.body.appendChild(divElement);
-//   })(
-//     window,
-//     document,
-//     "script",
-//     "spotbus-chatbot",
-//     "https://spotbus-chatbot.s3.amazonaws.com/main.js"
-//   )
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
