24cfeb3c2a86b79b7b027da67ee706bd98e91280
googleroboto <166996707+googleroboto@users.noreply.github.com>
2024-04-17 03:18:03 +0530

    Update googlefont.js

--- googlefont.js
+++ googlefont.js
@@ -6,29 +6,29 @@ root.style.color = "red";
 root.style.backgroundColor = "yellow";
 root.style.padding = "10px";
 
-(function (w, d, s, o, f, js, fjs) {
-    w["spotbus-chatbot"] = o;
-    w[o] =
-      w[o] ||
-      function () {
-        (w[o].q = w[o].q || []).push(arguments);
-      };
-    (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
-    js.id = o;
-    js.src = f;
-    js.async = 1;
-    fjs.parentNode.insertBefore(js, fjs);
-    w.chatbotConfig = {
-      token: "8fab2211f44a4a0993e2a8ca619a580f",
-      userid: "6ba27e00-cac7-46d4-ade1-e0cc23f65bd4",
-    };
-    const divElement = d.createElement("div");
-    divElement.id = "spotbus-chatbot-root";
-    d.body.appendChild(divElement);
-  })(
-    window,
-    document,
-    "script",
-    "spotbus-chatbot",
-    "https://spotbus-chatbot.s3.amazonaws.com/main.js"
-  )
+// (function (w, d, s, o, f, js, fjs) {
+//     w["spotbus-chatbot"] = o;
+//     w[o] =
+//       w[o] ||
+//       function () {
+//         (w[o].q = w[o].q || []).push(arguments);
+//       };
+//     (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+//     js.id = o;
+//     js.src = f;
+//     js.async = 1;
+//     fjs.parentNode.insertBefore(js, fjs);
+//     w.chatbotConfig = {
+//       token: "8fab2211f44a4a0993e2a8ca619a580f",
+//       userid: "6ba27e00-cac7-46d4-ade1-e0cc23f65bd4",
+//     };
+//     const divElement = d.createElement("div");
+//     divElement.id = "spotbus-chatbot-root";
+//     d.body.appendChild(divElement);
+//   })(
+//     window,
+//     document,
+//     "script",
+//     "spotbus-chatbot",
+//     "https://spotbus-chatbot.s3.amazonaws.com/main.js"
+//   )
